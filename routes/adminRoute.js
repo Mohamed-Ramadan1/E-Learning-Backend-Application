4d1860a686c24e1059aa2d44fcc95939dc0f3d03
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2024-06-04 15:28:32 +0300

    API completed successfully.

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,14 +1,6 @@
 import { Router } from 'express';
 import { siteStatics } from './../controllers/adminController.js';
 
-import {
-  approveFinancialAidRequest,
-  rejectFinancialAidRequest,
-  getAllFinancialAidRequestsAdmin,
-  getFinancialAidRequestAdmin,
-  deleteFinancialAidRequestAdmin,
-} from '../controllers/financialAidController.js';
-
 import { restrictTo, protect } from './../middleware/authMiddleware.js';
 
 const router = Router();
@@ -19,18 +11,5 @@ router.use(restrictTo('admin'));
 
 //statics
 router.route('/siteStatics').get(siteStatics);
-
-//Financial Aid Requests end points
-router
-  .route('/financialAidRequests/:id/approve')
-  .patch(approveFinancialAidRequest);
-router
-  .route('/financialAidRequests/:id/reject')
-  .patch(rejectFinancialAidRequest);
-router.route('/financialAidRequests').get(getAllFinancialAidRequestsAdmin);
-router.route('/financialAidRequests/:id').get(getFinancialAidRequestAdmin);
-router
-  .route('/financialAidRequests/:id')
-  .delete(deleteFinancialAidRequestAdmin);
 
 export default router;
--- routes/financialAidRoute.js
+++ routes/financialAidRoute.js
@@ -5,18 +5,36 @@ import {
   getFinancialAidRequests,
   updateFinancialAidRequest,
   deleteFinancialAidRequest,
+  ////////////////////////////////
+  approveFinancialAidRequest,
+  rejectFinancialAidRequest,
+  getAllFinancialAidRequestsAdmin,
+  getFinancialAidRequestAdmin,
+  deleteFinancialAidRequestAdmin,
 } from '../controllers/financialAidController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, restrictTo } from '../middleware/authMiddleware.js';
 
 const router = Router();
 router.use(protect);
 
 router.route('/').get(getFinancialAidRequests);
 router.route('/').post(createFinancialAidRequest);
+
+router
+  .route('/allRequests')
+  .get(restrictTo('admin'), getAllFinancialAidRequestsAdmin);
+
 router
   .route('/:id')
   .get(getFinancialAidRequest)
   .patch(updateFinancialAidRequest)
   .delete(deleteFinancialAidRequest);
+
+//Financial Aid Requests end points
+router.use(restrictTo('admin'));
+router.route('/request:id').get(getFinancialAidRequestAdmin);
+router.route('/:id/approve').patch(approveFinancialAidRequest);
+router.route('/:id/reject').patch(rejectFinancialAidRequest);
+router.route('/deleteRequest/:id').delete(deleteFinancialAidRequestAdmin);
 
 export default router;
